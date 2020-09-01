e5815b3a2eb6a300051c6a9ac6a3e2f8ceb29a05
Miguel <mlopez38@unm.edu>
2020-09-01 11:23:23 -0600

    Removed unnecessary functions from code

--- Capstone_Project/src/Capstone_Project.cpp
+++ Capstone_Project/src/Capstone_Project.cpp
@@ -11,23 +11,15 @@
  * Date: 8/27/20
  */
 
-#include <neopixel.h>
 #include <SPI.h>
 #include <SdFat.h>
 
 void setup();
 void loop();
-void soundtest();
-void buttonFunction();
-void microphones();
-void pixelBlack();
-void pixelGreen();
-#line 12 "c:/Users/User/Documents/IoT/Capstone-Project/Capstone_Project/src/Capstone_Project.ino"
-#define PIXEL_PIN A4
-#define PIXEL_COUNT 1
-#define PIXEL_TYPE WS2812B
-Adafruit_NeoPixel pixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
-
+void simplesoundtest();
+void getTimingOfSound();
+void getMicrophoneValues();
+#line 11 "c:/Users/User/Documents/IoT/Capstone-Project/Capstone_Project/src/Capstone_Project.ino"
 const int chipSelect = SS;
 
 #define FILE_BASE_NAME "Data"
@@ -44,24 +36,20 @@ int micro3 = A2;
 int val1;
 int val2;
 int val3; 
+int T1;
+int T2;
+int T3;
+
 
 int threshold = 4000;
 int startTime;
 int lastTime;
-
-int button = A3;
-int buttonState;
-
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
   Serial.begin(9600);
 
-  pixel.begin();
-  pixel.clear();
-
-  pinMode(button, INPUT);
   pinMode(micro1, INPUT);
   pinMode(micro2, INPUT);
   pinMode(micro3, INPUT);
@@ -70,50 +58,43 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  soundtest();
+  getTimingOfSound();
 }
 
-void soundtest()
+void simplesoundtest()
 {
-  microphones();
-  startTime = micros();
-  if(val1 > threshold || val2 > threshold || val3 > threshold)
-  {
-    Serial.printf("mic1 = %i | mic2 = %i | mic3 = %i | Time = %i\n",val1,val2,val3,lastTime);
-    lastTime = (micros() - startTime);
-  }
+  getMicrophoneValues();
+   if(val1 > threshold || val2 > threshold || val3 > threshold)
+   {
+     Serial.printf("mic1 = %i | mic2 = %i | mic3 = %i | Time = %i\n",val1,val2,val3);
+   }
 }
 
-void buttonFunction()
+void getTimingOfSound()
 {
-  buttonState = digitalRead(button);
-  if(buttonState == HIGH)
+  getMicrophoneValues();
+  while(T1 = 0 || T2 = 0 || T3 = 0)
   {
-    pixelGreen();
-    microphones();
+    if((T1 = 0 && val1 > threshold))
+    {
+      T1 = millis();
+    }
+    if((T2 = 0 && val2 > threshold))
+    {
+      T2 = millis();
+    }
+    if((T3 = 0 && val3 > threshold))
+    {
+      T3 = millis();
+    }
   }
-  else
-  {
-    pixelBlack();
-  }
+  Serial.printf("T1 = %i | T2 = %i | T3 = %i\n",T1, T2, T3);
 }
 
-void microphones()
+void getMicrophoneValues()
 {
   val1 = analogRead(micro1);
   val2 = analogRead(micro2);
   val3 = analogRead(micro3);
   //Serial.printf("Microphone 1 = %i | Microphone 2 = %i | Microphone 3 = %i\n", val1, val2, val3);
-}
-
-void pixelBlack()
-{
-  pixel.setPixelColor(0,0,0,0);
-  pixel.show();
-}
-
-void pixelGreen()
-{
-  pixel.setPixelColor(0,0,255,0);
-  pixel.show();
 }
